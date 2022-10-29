6f7404bfa583a64b67f2ff491d35ad8919840786
ItsJaime <108011836+ItsJaime@users.noreply.github.com>
2022-10-29 18:04:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const Discord = require('discord.js')
+const client = new Discord.Client({ intents: 32767 });
+const fs = require('fs')
 
+client.on('ready',() =>{
+  console.log('Estoy listo!')
+  client.application.commands.set([
+  {
+    name: 'ip',
+    description: 'Ip del servidor',
+    options: [],
+  }
+])
+});
+
+client.on('interactionCreate', (int) => {
+  if (int.isCommand() && int.commandName === 'ip') {
+    int.reply('La ip es mc.yoloretro.ml (𝙀𝙡 𝙨𝙚𝙧𝙫𝙞𝙙𝙤𝙧 𝙚𝙨𝙩𝙖 𝙚𝙣 𝙛𝙖𝙨𝙚 𝙗𝙚𝙩𝙖)')
+  }
+})
+
+client.login(OTcyODI3MzY0NDI3OTY4NTg0.G94G1C.ri2e6q4tueMzIINlp5bDzIXECCgWl2mH40Z5vw);
